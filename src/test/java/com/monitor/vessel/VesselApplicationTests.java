d4e86edec5cc23d5811614d698024831dc71e1d6
gruchh <kubol2@gmail.com>
2026-04-20 19:53:05 +0200

    Initial commit - Vessel Monitoring App

---
+++ src/test/java/com/monitor/vessel/VesselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitor.vessel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
